9a4c7b41ea2ed734d3cf0eeda90c54ee74c0c452
Paulo Lima <paulobruny@gmail.com>
2019-04-22 11:48:47 -0300

    initial config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckCircle, faTrashAlt)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
