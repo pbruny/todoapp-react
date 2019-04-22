87703dbaee2f65ecfafbfeda07b3d89884f2053f
Paulo Lima <paulobruny@gmail.com>
2019-04-22 12:25:34 -0300

    Update app with components and add scss style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container'
+
+import Header from './components/header/Header';
+import Tasks from './components/tasks/Tasks';
+
+import './App.scss';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheckCircle, faTrashAlt)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
+        <Container>
+          <Tasks/>
+        </Container>
       </div>
     );
   }
