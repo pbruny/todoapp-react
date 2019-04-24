be2265e3a30eb645b995eb804281f4be290ca830
Paulo Lima <paulobruny@gmail.com>
2019-04-24 14:30:41 -0300

    Updates

---
+++ precache-manifest.39fca1c21fe9f44ea003f81342ae6fe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  },
+  {
+    "revision": "b6166362c0384efbb9c6",
+    "url": "/todoapp/static/js/main.b48c4b46.chunk.js"
+  },
+  {
+    "revision": "febd164012a168331732",
+    "url": "/todoapp/static/js/2.ae3ff984.chunk.js"
+  },
+  {
+    "revision": "b6166362c0384efbb9c6",
+    "url": "/todoapp/static/css/main.5cda02fa.chunk.css"
+  },
+  {
+    "revision": "003fd04fa5516148efe07b959da8c07c",
+    "url": "/todoapp/index.html"
+  }
+];
