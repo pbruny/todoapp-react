c355677ec71b74b1970370c1a9448e40f122253e
Weslles <joseweslles_b2@hotmail.com>
2019-10-25 13:57:40 -0300

    REMOVE IMPORT SERVICE WORKER

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
